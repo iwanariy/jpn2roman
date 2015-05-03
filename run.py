affa6de994d6994a08aef9def67ca58d5ed9e2da
narikin <yuki.iwnr@gmail.com>
2015-05-03 14:26:08 +0900

    Extract data from response in xml

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 #!/bin/python2.7
 # coding: utf-8
 import requests
+from xml.dom.minidom import parseString
 import os
 import dotenv
 dotenv.load_dotenv(".env")
@@ -21,5 +22,18 @@ if __name__ == '__main__':
     r = requests.post(URL, data=payload)
 
     # Show responce
-    print u"text: %s" % r.text
-    print u"encoding: %s" % r.encoding
+    # print u"text: %s" % r.text
+    # print u"encoding: %s" % r.encoding
+
+    # from xml.dom.minidom import parse, parseString
+    doml = parseString(r.content)
+
+    # Get Word DOM
+    results = doml.getElementsByTagName('Result')
+    word_lists = results.item(0).getElementsByTagName('WordList')
+    words = word_lists.item(0).getElementsByTagName('Word')
+
+    for word in words:
+        print word.getElementsByTagName('Surface').item(0).childNodes[0].data
+        print word.getElementsByTagName('Furigana').item(0).childNodes[0].data
+        print word.getElementsByTagName('Roman').item(0).childNodes[0].data
