a1e428e4f47cd9d1e0a9a837923010ee484f5475
narikin <yuki.iwnr@gmail.com>
2015-05-06 21:34:33 +0900

    Add test_1()

--- jpn2roman/tests/test_jpn2roman.py
+++ jpn2roman/tests/test_jpn2roman.py
@@ -9,8 +9,17 @@ class TestJpn2roman(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_hello_world(self):
-        self.assertTrue(True)
+    def test_1(self):
+        # Get result
+        results = get_roman_from_jpn(u"山田太郎")
+
+        # Expected
+        expected_0 = (u"山田", u"やまだ", u"yamada")
+        expected_1 = (u"太郎", u"たろう", u"tarou")
+
+        # Assertion
+        self.assertEqual(expected_0, results[0])
+        self.assertEqual(expected_1, results[1])
 
 
 if __name__ == "__main__":
