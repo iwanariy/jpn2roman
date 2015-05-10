a6eddbc4a06611072a2b8f1940aebbca82febe5b
narikin <yuki.iwnr@gmail.com>
2015-05-10 22:03:27 +0900

    Add test including space

--- jpn2roman/tests/test_jpn2roman.py
+++ jpn2roman/tests/test_jpn2roman.py
@@ -21,6 +21,20 @@ class TestJpn2roman(unittest.TestCase):
         self.assertEqual(expected_0, results[0])
         self.assertEqual(expected_1, results[1])
 
+    def test_include_space(self):
+        # Get result
+        results = get_roman_from_jpn(u"山田　太郎")
+
+        # Expected
+        expected_0 = (u"山田", u"やまだ", u"yamada")
+        expected_1 = (u"　", u"　", u"　")
+        expected_2 = (u"太郎", u"たろう", u"tarou")
+
+        # Assertion
+        self.assertEqual(expected_0, results[0])
+        self.assertEqual(expected_1, results[1])
+        self.assertEqual(expected_2, results[2])
+
 
 if __name__ == "__main__":
     unittest.main()
